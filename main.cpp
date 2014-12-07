5a0dfef1ff0f860bdf933eb95416c5949e7201d0
lyase damasked <lyase.d@gmail.com>
2014-12-07 18:27:38 +0100

    fix streaming sound file tested in server ok

--- main.cpp
+++ main.cpp
@@ -60,9 +60,10 @@ int main(int, char**) {
     namespace cmd = agi_proxy::command;
     std::vector<agi_proxy::CommandRunner> commands;
     commands.emplace_back(new cmd::Answer(p));
+
     commands.emplace_back(new cmd::SayNumber(p, 123456789));
     commands.emplace_back(new cmd::ChannelStatus(p));
-    commands.emplace_back(new cmd::StreamFile(p, "beep"));
+    commands.emplace_back(new cmd::StreamFile(p, "testagi"));
     
     // Run the commands
     try {
